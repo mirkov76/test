f989fcce92ead325008b07ed217bb6f743572c6c
mirkov76 <mirkosamuel76@gmail.com>
2024-09-18 21:34:47 -0300

    creación de items

---
+++ Content/Items/faka.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace test.Content.Items
+{ 
+
+	public class faka : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.test.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 6;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.IronBar, 15);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
